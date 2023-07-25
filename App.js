8db54003843a8b66bd365984ebc0af335edf7375
Oyekan Bamidele Olayinka <oyekanolayinka1@gmail.com>
2023-07-25 15:34:30 +0100

    initial setup

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{
+        color: 'green',
+        fontWeight: 'bold',
+        fontSize: 32
+      }}>my name is Oyekan Olayinka and i am a React Native Developer</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
